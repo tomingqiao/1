0f976bdac8fc15ed2c558d54538b2067f073b221
tomingqiao <3107099063@qq.com>
2022-03-24 21:02:36 +0800

    2022324

--- java代码/s/Ssy.java
+++ java代码/s/Ssy.java
@@ -2,8 +2,9 @@
  * Ssy
  */
 public class Ssy {
+    static String girl = "boy";
 
     public static void main(String[] args) {
-        System.out.println("ddddddddddd");
+        System.out.println("ddddddddddd" + girl);
     }
 }
