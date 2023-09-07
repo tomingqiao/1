cd890616745c2f6b6337f9c2153bad3774fed6d1
tomingqiao <3107099063@qq.com>
2023-09-07 19:40:38 +0800

    1

--- java编程/ss/Aaaa.java
+++ java编程/ss/Aaaa.java
@@ -3,13 +3,13 @@ package ss;
 public class Aaaa {
 
 	public static void main(String[] args) {
-		char x = 'Äã', y = 'e', z = '³Ô';
+		char x = 'a', y = 'e', z = 'b';
 		if (x > 'A') {
-			y = 'Æ»';
-			z = '¹û';
+			y = 'C';
+			z = 'D';
 		} else
-			y = 'Ëá';
-		z = 'Ìð';
+			y = 'c';
+		z = 'm';
 		System.out.println(x + "," + y + "," + z);
 	}
 
